9c750639c8b8caca93ef89a27adb0b2509ebc6df
Manel Matougui <m.matougui@gmail.com>
2019-12-25 16:56:10 -0800

    add levelSelector4

---
+++ MovingBlocks/Buttons/PrevLevelButton3.swift
@@ -0,0 +1,30 @@
+//
+//  PrevLevelButton3.swift
+//  MovingBlocks
+//
+//  Created by Manel matougui on 12/25/19.
+//  Copyright © 2019 Manel matougui. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+class PrevLevelButton3 : SKSpriteNode {
+    // initialize playButton
+    // we need constructor to enable user interaction
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        isUserInteractionEnabled = true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>,with event: UIEvent?) {
+        print("button pressed")
+        if let levels = SKScene(fileNamed: "LevelsSelector3"){
+            // Set the scale mode to scale to fit the window
+            levels.scaleMode = .aspectFill
+            // Present the scene
+            scene?.view?.presentScene(levels)
+        }
+        
+    }
+}
+
